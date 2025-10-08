b3b15744ec37d8a65983620e8355aab0b74aae8b
Chinthaka Kasun <chnthkksn@gmail.com>
2025-10-08 11:25:03 +0530

    update hosts

---
+++ src/config/hosts.js
@@ -0,0 +1,11 @@
+const hosts = {
+  sg: {
+    ip: "0.0.0.0",
+    user: "root",
+    password: "root",
+    path: "/etc/x-ui/x-ui.db",
+    table: "client_traffics",
+  },
+};
+
+export default hosts;
